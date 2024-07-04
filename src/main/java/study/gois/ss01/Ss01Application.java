f22f86cc6480ea45da8c9e677d5652b3bec9b14e
nableggois <gustavo.gois@n-able.com>
2024-07-04 18:04:28 +0100

    Initial commit

---
+++ src/main/java/study/gois/ss01/Ss01Application.java
@@ -0,0 +1,13 @@
+package study.gois.ss01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss01Application.class, args);
+    }
+
+}
