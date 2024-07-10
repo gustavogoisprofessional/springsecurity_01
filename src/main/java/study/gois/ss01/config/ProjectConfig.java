b8d3e29e62d245288a19f70de0bce0a4cc306898
nableggois <gustavo.gois@n-able.com>
2024-07-10 22:02:38 +0100

    Ch02-ex03

--- src/main/java/study/gois/ss01/config/ProjectConfig.java
+++ src/main/java/study/gois/ss01/config/ProjectConfig.java
@@ -2,15 +2,44 @@ package study.gois.ss01.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+
     @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        // App uses HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // All the requests require authentication.
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+                // With a slight change, you can make all the endpoints accessible without the need for credentials
+                // c -> c.anyRequest().permitAll()
+        );
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        var userDetailsService = new InMemoryUserDetailsManager(user);
+
+        http.userDetailsService(userDetailsService);
+
+        return http.build();
+    }
+
+    /*@Bean
     UserDetailsService userDetailsService() {
 
         var user = User.withUsername("john")
@@ -19,7 +48,7 @@ public class ProjectConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(user);
-    }
+    }*/
 
     @Bean
     PasswordEncoder passwordEncoder() {
